e7f19c139ee69f021ba7ca9897fe11a83134ee5c
jooa7878 <jooa7878@naver.com>
2022-05-26 16:09:36 +0900

    added items upload, live pages

--- pages/live/create.tsx
+++ pages/live/create.tsx
@@ -1,27 +1,22 @@
 import type { NextPage } from "next";
 
-const Upload: NextPage = () => {
+const Create: NextPage = () => {
   return (
-    <div className="px-4 py-16 space-y-5">
+    <div className="py-10 px-4 space-y-5">
       <div>
-        <label className="w-full h-48 flex items-center justify-center border-2 border-dashed border-gray-300 py-6 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500 cursor-pointer">
-          <svg
-            className="h-12 w-12"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-
-          <input className="hidden" type="file" />
+        <label
+          htmlFor="name"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          Name
         </label>
+        <div className="rounded-md shadow-sm relative flex items-center">
+          <input
+            id="name"
+            type="text"
+            className="appearance-none w-full px-3 py-2 pl-7 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+          />
+        </div>
       </div>
       <div>
         <label
@@ -56,10 +51,10 @@ const Upload: NextPage = () => {
         />
       </div>
       <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product
+        Go Live
       </button>
     </div>
   );
 };
 
-export default Upload;
+export default Create;
